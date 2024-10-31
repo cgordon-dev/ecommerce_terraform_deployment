65de59a0d4cc7bdac8966c58444a2fd8f1f64dc8
Carl Gordon <cgordon.dev@gmail.com>
2024-10-30 23:39:44 -0400

    Update settings.py

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
          'PASSWORD': 'your_password',
          'HOST': 'your-rds-endpoint.amazonaws.com',
          'PORT': '5432',
-#     }, 
+     }, 
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
