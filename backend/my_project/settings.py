0f6770c1c237c401a741f88e65f7aeb902931ec6
Carl Gordon <cgordon.dev@gmail.com>
2024-10-30 23:33:09 -0400

    Update settings.py

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
 
-        'NAME': 'your_db_name',
-        'USER': 'your_username',
-        'PASSWORD': 'your_password',
-        'HOST': 'your-rds-endpoint.amazonaws.com',
-        'PORT': '5432',
-    }, 
+#         'NAME': 'your_db_name',
+#         'USER': 'your_username',
+#         'PASSWORD': 'your_password',
+#         'HOST': 'your-rds-endpoint.amazonaws.com',
+#         'PORT': '5432',
+#     }, 
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
